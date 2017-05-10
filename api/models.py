22c62896aace9737f7285da4f05d0bdfe6e8c171
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-10 18:04:39 -0400

    Add details JSON field to resources model/table

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 class Resource(models.Model):
   type = models.CharField(max_length=50)
+  details = JSONField(default=dict)
