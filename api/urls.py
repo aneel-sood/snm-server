491a5abcc7cdaa56ef522457e89e3f51b4b1c1f0
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-11 17:46:22 -0400

    Add test API endpoint for returning JSON of all interpreter resources

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from api import views
+
+urlpatterns = [
+    url(r'^interpreters/$', views.interpreter_list),
+    # url(r'^$', views.index, name='index'),
+    # url(r'^interpreters/(?P<pk>[0-9]+)/$', views.interpreter_detail),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from api.models import Resource
+from api.serializers import ResourceSerializer
+
+@csrf_exempt
+def interpreter_list(request):
+    if request.method == 'GET':
+        resources = Resource.objects.all()
+        serializer = ResourceSerializer(resources, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    # elif request.method == 'POST':
+    #     data = JSONParser().parse(request)
+    #     serializer = ResourceSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data, status=201)
+    #     return JsonResponse(serializer.errors, status=400)
