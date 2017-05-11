b4bdf6ca7fc5d90f91f8ee8ee3c3cade05f43409
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-11 19:19:13 -0400

    Include provider fields in resource serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from api.models import Resource
+from api.models import Provider, Resource
+
+class ProviderSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Provider
+    fields = ('first_name', 'last_name', 'email')
 
 class ResourceSerializer(serializers.ModelSerializer):
+  provider = ProviderSerializer(read_only='True')
   class Meta:
       model = Resource
-      fields = ('id', 'type', 'details')
+      fields = ('id', 'type', 'details', 'provider')
