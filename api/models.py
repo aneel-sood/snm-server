44ebdb50f0f7979c8288fcd41469f9561520f3fe
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-11 16:15:44 -0400

    Add Provider DB table and corresponding model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+class Provider(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=60)
+  email = models.CharField(max_length=100)
+
 class Resource(models.Model):
   type = models.CharField(max_length=50)
   details = JSONField(default=dict)
+  provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
