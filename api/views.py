7f0ebf417826fbabc26be50e8532d7ea64f53e65
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-22 14:02:31 -0400

    restructured URL params JSON for resources end point

--- api/views.py
+++ api/views.py
@@ -10,16 +10,15 @@ from django.db.models import Q
 @csrf_exempt
 def resources(request):
     if request.method == 'GET':
-        type = request.GET.get('type', '')
-        details = loads(request.GET.get('details', '{}'))
-        
-        q_objects = Q()
-        for param_name, value in details.items():
-          q_objects.add(Q(**{'{0}__{1}'.format('details', param_name): value}), Q.AND)
+      params = loads(request.GET.get('params', '{}'))
+      
+      q_objects = Q()
+      for param_name, value in params['details'].items():
+        q_objects.add(Q(**{'{0}__{1}'.format('details', param_name): value}), Q.AND)
 
-        resources = Resource.objects.filter(type=type).filter(q_objects)
-        serializer = ResourceSerializer(resources, many=True)
-        return JsonResponse(serializer.data, safe=False)
+      resources = Resource.objects.filter(type=params['type']).filter(q_objects)
+      serializer = ResourceSerializer(resources, many=True)
+      return JsonResponse(serializer.data, safe=False)
 
     # elif request.method == 'POST':
     #     data = JSONParser().parse(request)
