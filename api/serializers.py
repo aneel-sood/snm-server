bc6d3900c20b562f6e1684e5fcc5d1e00b664874
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-25 17:14:01 -0400

    Provider serializer should include data for related resources

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from api.models import Provider, Resource
 
-class ProviderSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Provider
-    fields = ('first_name', 'last_name', 'email')
-
 class ResourceSerializer(serializers.ModelSerializer):
-  provider = ProviderSerializer(read_only='True')
   class Meta:
       model = Resource
-      fields = ('id', 'type', 'details', 'provider')
+      fields = ('type', 'details')
+
+class ProviderSerializer(serializers.ModelSerializer):
+  resources = ResourceSerializer(many=True)
+  class Meta:
+    model = Provider
+    fields = ('id', 'first_name', 'last_name', 'email', 'resources')
