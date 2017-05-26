2eb99c8aa535a60ecad99b5e8100d5c3b008ef04
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-05-25 20:50:19 -0400

    create Client model and related migration

--- api/models.py
+++ api/models.py
@@ -10,3 +10,8 @@ class Resource(models.Model):
   type = models.CharField(max_length=50)
   details = JSONField(default=dict)
   provider = models.ForeignKey(Provider, related_name='resources', on_delete=models.CASCADE)
+
+class Client(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=60)
+  email = models.CharField(max_length=100)
