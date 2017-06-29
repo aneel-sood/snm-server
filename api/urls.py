7533aaac276756998cd7db037361ae68c4ecb2d2
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2017-06-29 09:54:35 -0400

    Add post/create process to client end point

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^providers/$', views.providers),
     url(r'^clients/$', views.clients),
     url(r'^dashboard/clients/$', views.dashboard_clients),
+    url(r'^client/$', views.client),
     url(r'^client/(?P<pk>[0-9]+)/$', views.client),
     url(r'^client/(?P<client_id>[0-9]+)/needs/$', views.client_needs),
     url(r'^client/(?P<client_id>[0-9]+)/need/(?P<pk>[0-9]+)/$', views.client_need),
